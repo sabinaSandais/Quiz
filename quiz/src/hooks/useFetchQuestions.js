c8699470bf060eca8926f83f15be0a6349668d24
sabinaSandais <129327140+sabinaSandais@users.noreply.github.com>
2024-08-09 01:13:02 +0200

    Create useFetchQuestions.js

---
+++ quiz/src/hooks/useFetchQuestions.js
@@ -0,0 +1,27 @@
+import { useState, useEffect } from "react";
+import { fetchQuestions } from "../api";
+
+const useFetchQuestions = (difficulty) => {
+  const [questions, setQuestions] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const loadQuestions = async () => {
+      try {
+        const fetchedQuestions = await fetchQuestions(difficulty);
+        setQuestions(fetchedQuestions);
+      } catch (error) {
+        setError("Failed to load questions. Please try again.");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    loadQuestions();
+  }, [difficulty]);
+
+  return { questions, loading, error };
+};
+
+export default useFetchQuestions;
