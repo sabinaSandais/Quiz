782d4d3bda7732dd058ef55d93000f842f9a6083
sabinaSandais <129327140+sabinaSandais@users.noreply.github.com>
2024-08-08 08:18:01 +0200

    Update ResultPage.js

--- quiz/src/components/ResultPage.js
+++ quiz/src/components/ResultPage.js
@@ -1,14 +1,17 @@
 import React, { useContext } from 'react';
 import { QuizContext } from '../QuizContext';
+import './ResultPage.css';
 
 const ResultPage = () => {
   const { finalScore, resetQuiz } = useContext(QuizContext);
 
   return (
     <div className="result-page">
+    
       <h1>Quiz Completed</h1>
       <p>Your final score is {finalScore} out of 10</p>
-      <button onClick={resetQuiz}>Try Again</button>
+      <button className='reset' onClick={resetQuiz}>Try Again</button>
+    
     </div>
   );
 };
