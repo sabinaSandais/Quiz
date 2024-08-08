b56dcf4f0f1a29e57ea021ea163072ee602e93ec
sabinaSandais <129327140+sabinaSandais@users.noreply.github.com>
2024-08-08 23:42:33 +0200

    Update score.test.js

--- quiz/src/--tests--/score.test.js
+++ quiz/src/--tests--/score.test.js
@@ -1,14 +1,13 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect"; // for the toBeInTheDocument matcher
+import Score from "../components/Score";
 
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect'; // for the toBeInTheDocument matcher
-import Score from './Score';
-
-test('renders Score component with the given score', () => {
+test("renders Score component with the given score", () => {
   const testScore = 2; // Example score value
-  
+
   render(<Score score={testScore} />);
-  
+
   // Check that the text is rendered correctly
   expect(screen.getByText(`Score: ${testScore}`)).toBeInTheDocument();
 });
