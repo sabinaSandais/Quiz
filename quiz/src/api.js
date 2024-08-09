7490cdf7c6bcb75954bcafb22474a4a5709f1293
sabinaSandais <129327140+sabinaSandais@users.noreply.github.com>
2024-08-09 08:39:41 +0200

    Update api.js

--- quiz/src/api.js
+++ quiz/src/api.js
@@ -1,14 +1,16 @@
-import axios from "axios";
+// import axios from "axios";
 
-export const fetchQuestions = async (difficulty) => {
-  try {
-    const response = await axios.get(
-      `https://opentdb.com/api.php?amount=10&difficulty=${difficulty}`
-    );
-    console.log(response.data.results);
-    return response.data.results;
-  } catch (error) {
-    console.error("Error fetching questions:)", error);
-    throw error;
-  }
-};
+// export const fetchQuestions = async (difficulty) => {
+//   try {
+//     const response = await axios.get(
+//       `https://opentdb.com/api.php?amount=10&difficulty=${difficulty}`
+//     );
+//     console.log(response.data.results);
+//     return response.data.results;
+//   } catch (error) {
+//     console.error("Error fetching questions:)", error);
+//     throw error;
+//   }
+// };
+// api.js
+export const BASE_URL = "https://opentdb.com/api.php";
